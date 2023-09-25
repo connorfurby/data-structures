2d1a684864ecec6d86d8fb3cf0e439964ba988bd
connorfurby <cafurby@stu.naperville203.org>
2023-09-25 13:17:11 -0500

    9/25 sudoku progress

--- Chapter 15 Activities/Sudoku/src/SudokuSolver.java
+++ Chapter 15 Activities/Sudoku/src/SudokuSolver.java
@@ -3,13 +3,13 @@ import java.io.FileNotFoundException;
 import java.util.*;
 
 public class SudokuSolver {
-    private final int M = 3;
-    private final int N = M * M;
-    private int[][] grid;
-    private ArrayList<Set<Integer>> rows;
-    private ArrayList<Set<Integer>> cols;
-    private ArrayList<Set<Integer>> squares;
-    private Set<Integer> nums;
+    private final int M = 3; // number of squares in a row
+    private final int N = M * M; // number of squares in a sudoku
+    private int[][] grid; // 2d array of integers 
+    private ArrayList<Set<Integer>> rows; // stores the rows
+    private ArrayList<Set<Integer>> cols; // stores the columns
+    private ArrayList<Set<Integer>> squares; // stores all of the squares
+    private Set<Integer> nums; // populates the potential numbers
 
     public SudokuSolver(String fileName) {
         // read the puzzle file
@@ -23,7 +23,7 @@ public class SudokuSolver {
                 for (int col = 0; col < N; col++) {
                     String strVal = line.substring(col, col + 1);
                     int number;
-                    if (strVal.equals("x")) {
+                    if (strVal.equals("x")) {  // blank = 0
                         number = 0;
                     } else {
                         number = Integer.parseInt(strVal);
@@ -35,11 +35,29 @@ public class SudokuSolver {
             System.out.println("Cannot open: " + fileName);
         }
 
-        // create the list of sets for each row (this.rows)
-        // ...
+        // create the list of sets for each row (this.rows) 
+        this.rows = new ArrayList<Set<Integer>>();
+        for (int i = 0; i < N; i++)
+        {
+            Set <Integer> currRow = new HashSet<>(); 
+            for (int k = 0; k < N; k++)
+            {
+                currRow.add(grid[i][k]);
+            }
+            this.rows.add(currRow);
+        }
 
         // create the list of sets for each col (this.cols)
-        // ...
+        this.cols = new ArrayList<Set<Integer>>();
+        for (int i = 0; i < 9; i++)
+        {
+            Set <Integer> currCol = new HashSet<>(); 
+            for (int k = 0; k < 9; k++)
+            {
+                currCol.add(grid[k][i]);
+            }
+            this.cols.add(currCol);
+        }
 
         // create the list of sets for each square (this.squares)
         /* the squares are added to the list row-by-row:
@@ -47,10 +65,31 @@ public class SudokuSolver {
             3 4 5
             6 7 8
          */
-        // ...
+        this.squares = new ArrayList<Set<Integer>>();
+        for (int sqnum = 0; sqnum < N; sqnum++)
+        {
+            Set <Integer> currSquare = new HashSet<>();
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    if (sqnum < N/3)
+                        currSquare.add(grid[((sqnum % 3)* 3) + row][col]);
+                    else if (sqnum < N/3*2)
+                        currSquare.add(grid[((sqnum % 3)* 3) + row][col + 3]);
+                    else
+                        currSquare.add(grid[((sqnum % 3)* 3) + row][col + 6]);
+                }
+            }
+            this.squares.add(currSquare);
+        }
 
         // create a hash set for [1..9] (this.nums)
-        // ...
+        this.nums = new HashSet<>();
+        for (int i = 0; i < 9; i++)
+        {
+            this.nums.add(i);
+        }
 
         // visually inspect that all the sets are correct
         for (int row = 0; row < N; row++) {
@@ -141,7 +180,7 @@ public class SudokuSolver {
     }
 
     public static void main(String[] args) {
-        String fileName = "src/puzzle1.txt";
+        String fileName = "Chapter 15 Activities/Sudoku/src/puzzle1.txt";
 
         SudokuSolver solver = new SudokuSolver(fileName);
         System.out.println(solver);
