dbf1518b656867557d3dbcfef0965b3c9b6a78a2
connorfurby <cafurby@stu.naperville203.org>
2023-09-29 13:16:21 -0500

    finished gradebook

--- Chapter 15 Class Notes/src/MapDemo.java
+++ Chapter 15 Class Notes/src/MapDemo.java
@@ -11,5 +11,27 @@ public class MapDemo
 {
     public static void main(String[] args)
     {
+        /* The map interface is generic
+         * The first type is the type of the key.
+         * The second type is the type of the value */
+        Map <String, Color> favColors = new HashMap<>();
+
+        favColors.put("Hyder", Color.RED);
+        favColors.put("Athena", Color.MAGENTA);
+        favColors.put("Bavya", Color.BLUE);
+
+        // Two different keys can have the same value
+        favColors.put("Mitchell", Color.RED);
+
+        // The same key CANNOT have two different values
+        favColors.put("Athena", Color.ORANGE);
+
+        System.out.println(favColors.get("Athena"));
+
+        // Create a set of the keys in a map
+        Set<String> keys = favColors.keySet();
+        for (String key : keys) {
+            System.out.println(key + "(" + key.hashCode() + "): " + favColors.get(key));
+        }
     }
 }
