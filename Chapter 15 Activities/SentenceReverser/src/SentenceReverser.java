73412fe873c73691f589e5ce31ce94c41d2f0538
connorfurby <cafurby@stu.naperville203.org>
2023-10-06 12:29:33 -0500

    h

--- Chapter 15 Activities/SentenceReverser/src/SentenceReverser.java
+++ Chapter 15 Activities/SentenceReverser/src/SentenceReverser.java
@@ -15,14 +15,37 @@ public class SentenceReverser
     public static String reverse(String sentence)
     {
     	Scanner scanner = new Scanner(sentence);
-    	
-        // Complete this method. Use a Stack.
-        ...
-
-
-
-
-
+    	Stack<String> words = new Stack<>();
+        String newsentence = "";
+        int space1index = 0;
+        String currWord;
+        for (int i = 0; i < sentence.length(); i++)
+        {
+            if (sentence.charAt(i) == ' ' || sentence.charAt(i) == '.')
+            {
+                if (sentence.charAt(i) == '.')
+                    currWord = sentence.substring(space1index, i);
+                else
+                    currWord = sentence.substring(space1index, i + 1);
+                space1index = i;
+                words.push(currWord);
+            }
+            if (sentence.charAt(i) == '.')
+            {
+                boolean first = true;
+                while(words.size() > 0)
+                {
+                    currWord = words.pop();
+                    if (first)
+                        currWord.toUpperCase();
+                    if (words.size() == 1)
+                        currWord.toLowerCase();
+                    first = false;
+                    newsentence = newsentence + currWord;
+                }
+            }
+        }
+        return newsentence;
 
     }
 }
