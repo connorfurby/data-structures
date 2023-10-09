276b8cc3e4e5ae3068fef4f68d4561bc76acb61b
connorfurby <cafurby@stu.naperville203.org>
2023-10-09 13:17:59 -0500

    h

--- Chapter 15 Activities/SentenceReverser/src/SentenceReverser.java
+++ Chapter 15 Activities/SentenceReverser/src/SentenceReverser.java
@@ -17,19 +17,26 @@ public class SentenceReverser
     	Scanner scanner = new Scanner(sentence);
     	Stack<String> words = new Stack<>();
         String newsentence = "";
-        int space1index = 0;
+        int space1index = -1;
         String currWord;
         for (int i = 0; i < sentence.length(); i++)
         {
             if (sentence.charAt(i) == ' ' || sentence.charAt(i) == '.')
             {
                 if (sentence.charAt(i) == '.')
-                    currWord = sentence.substring(space1index, i);
-                else
-                    currWord = sentence.substring(space1index, i + 1);
-                space1index = i;
+                {
+                    currWord = sentence.substring(space1index + 1, i);
+                    currWord = currWord + " ";
+                }
+                else 
+                    currWord = sentence.substring(space1index + 1, i + 1);
+                if (sentence.charAt(i) == ' ')
+                    space1index = i;
+                else if (!(i + 1 >= sentence.length()))
+                    space1index = i + 1;
                 words.push(currWord);
             }
+            
             if (sentence.charAt(i) == '.')
             {
                 boolean first = true;
@@ -37,12 +44,14 @@ public class SentenceReverser
                 {
                     currWord = words.pop();
                     if (first)
-                        currWord.toUpperCase();
-                    if (words.size() == 1)
-                        currWord.toLowerCase();
+                        currWord = currWord.substring(0, 1).toUpperCase() + currWord.substring(1);
+                    if (words.size() == 0)
+                        currWord = currWord.toLowerCase();
                     first = false;
                     newsentence = newsentence + currWord;
                 }
+                space1index += 2;
+                newsentence = newsentence + ".";
             }
         }
         return newsentence;
